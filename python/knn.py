17798f4058de8b5507bf75a7880de679a749cb9b
Folorunsho Solomon Opeyemi <solomonope@gmail.com>
2013-09-16 09:52:33 +0100

    update

--- python/knn.py
+++ python/knn.py
@@ -15,6 +15,14 @@ def classify0(inX,dataset,labels,k):
     dataSetSize = dataset.shape[0];
     diffmat = numpy.tile(inX,(dataSetSize,1)) - dataset;
     sqDiffMat = diffmat ** 2;
+    sqDistances = sqDiffMat.sum(axis = 1);
+    distances   =  sqDistances** 0.5;
+    sortedDistIndices = distances.argsort();
+    classCount = {};
     
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndices[i]];
+        classCount[voteIlabel]  = classCount.get(voteIlabel,0) + 1;
     
-    return;
+    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1), reverse=True);
+    return sortedClassCount[0][0];
--- python/testknn.py
+++ python/testknn.py
@@ -8,3 +8,7 @@ print(group);
 
 
 print(labels);
+
+label  = knn.classify0([5,0],group,labels,3);
+
+print(label);
