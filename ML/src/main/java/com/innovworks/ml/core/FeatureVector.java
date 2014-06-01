a47dcbf59c88e452bfa18671c60553f6e0a3503d
Folorunsho Solomon Opeyemi <solomonope@gmail.com>
2014-06-01 21:22:06 +0100

    added calculator

---
+++ ML/src/main/java/com/innovworks/ml/core/FeatureVector.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.innovworks.ml.core;
+
+import com.innovworks.ml.math.Vector;
+
+/**
+ *
+ * @author Folorunsho Solomon
+ */
+public class FeatureVector {
+
+    private String label;
+    private int labelInt;
+    private Vector vector;
+
+}
---
+++ ML/src/main/java/com/innovworks/ml/math/Vector.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.innovworks.ml.math;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Folorunsho Solomon
+ */
+public class Vector extends ArrayList {
+
+    public void add(double value) {
+        this.add(value);
+    }
+
+}
