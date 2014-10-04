aa9ef3bc8fcfd3d33da075fbdcba41a3c27985ac
Folorunsho Solomon Opeyemi <solomonope@gmail.com>
2014-10-04 23:46:01 +0100

    added calculator

---
+++ selfie/src/main/java/com/innovworks/selfie/App.java
@@ -0,0 +1,13 @@
+package com.innovworks.selfie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
